d6a9becb638e9f1d2de9b6ecb82a7b8206caefff
NikitaXodar <nikixodr144@gmail.com>
2020-05-10 23:09:11 +0500

    Second Commit for WeatherForecast

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Ufa&appid=8cb414b6309886a3b93b59e9f1b5695b')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Miami&appid=8cb414b6309886a3b93b59e9f1b5695b')
     .then(function(resp) {
         return resp.json();
     })
@@ -10,6 +10,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Ufa&appid=8cb414b630988
         let cityBlock = document.querySelector('.city'),
             timeBlock = document.querySelector('.time-block'),
             descrWeather = document.querySelector('.descr'),
+            humidityWeather = document.querySelector('.humidity-info'),
             temp = document.querySelector('.temp'),
             pressure = document.querySelector('.descr-title-pressure');
             //getTimes = data.dt,
@@ -33,6 +34,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Ufa&appid=8cb414b630988
         cityBlock.innerHTML = data.name;
         pressure.innerHTML = Math.floor(data.main['pressure'] / 1.345);
         temp.innerHTML = "+" + Math.round(data.main['temp'] - 273,15) + "&deg;";
+        humidityWeather.innerHTML = data.main['humidity'] + "%";
         let weatherDescrInfo = data.weather[0]['main'];
 
         switch(weatherDescrInfo) {
@@ -47,29 +49,39 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Ufa&appid=8cb414b630988
         }
         document.querySelector('.image').innerHTML = '<img src="https://openweathermap.org/img/wn/' + data.weather[0]['icon'] +'@2x.png" alt="img-weather" class="image-block">';
 
-
-
-
-        let timer1 = document.querySelector('.timer');
-
-        function getDateFunc(time) {
-            let minutes1 = (time / (1000 * 60)) % 60,
-                hours1 = (time / (1000 * 60 * 60)) % 24;
-                console.log(typeof(minutes1) + " " + typeof(hours1));
-
-            if(minutes1 < 10) {
-                minutes1 = "0" + minutes1;
+        
+        // $('.info').on('click', function() {
+        //     $('.default-hidden').show(150);
+        // });
+        $( ".info" ).on('click', function() {     
+            if($('.default-hidden:visible').length) {
+                $('.default-hidden').hide(150);
+                document.querySelector('.info').innerHTML = "Подробнее&#9660;";
             }
-            if(hours1 < 10) {
-                hours1 = "0" + hours1;
+            else {
+                $('.default-hidden').show(150);
+                document.querySelector('.info').innerHTML = "Скрыть&#9650;";
             }
-            return `${hours1}:${minutes1}`;
-        }
-        timer1.innerHTML = getDateFunc(data.dt);
+        });
 
 
 
+        // let timer1 = document.querySelector('.timer');
 
+        // function getDateFunc(time) {
+        //     let minutes1 = Math.floor((time / (100 * 60)) % 60),
+        //         hours1 = Math.floor((time / (100 * 60 * 60)) % 24);
+        //         console.log(typeof(minutes1) + " " + typeof(hours1));
+
+        //     if(minutes1 < 10) {
+        //         minutes1 = "0" + minutes1;
+        //     }
+        //     if(hours1 < 10) {
+        //         hours1 = "0" + hours1;
+        //     }
+        //     return `${hours1}:${minutes1}`;
+        // }
+        // timer1.innerHTML = getDateFunc(data.dt);
 
     })
     .catch(function() {
